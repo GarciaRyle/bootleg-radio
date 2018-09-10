8c04764f78d4da7ef8206fc0fb2a9a938a744a9a
arielle <ariellevalmonte@gmail.com>
2018-09-10 15:39:38 +0800

    arielle modified changes

--- bootleg-radio/resources/views/welcome.blade.php
+++ bootleg-radio/resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    
+                    Try the changes
                 </div>
 
                 <div class="links">
