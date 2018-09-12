05fbb2dc177fb8a240aee8b640f9b857ff784ff7
Ryle <garciaryle@gmail.com>
2018-09-12 15:01:45 +0800

    added files to project

--- bootleg-radio/resources/views/welcome.blade.php
+++ bootleg-radio/resources/views/welcome.blade.php
@@ -1,95 +1,182 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel HEY WASSUP HELLO</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz" rel="stylesheet">
+    <title> Mondrianizm</title>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    
-                </div>
-                Bootleg-Radio
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<body bgcolor="#333539">
+    <div class="wrapper"> 
+       
+        <div class="box1">
+            <div id="slider">
+                <figure>
+                    <img src="images/photo1.jpg" alt>
+                    <img src="images/bk.jpg" alt>
+                    <img src="images/photo3.jpg" alt>
+                    <img src="images/photo4.jpg" alt>
+                    <img src="images/back.jpg" alt>
+                </figure>
             </div>
         </div>
+        <div class="box2">
+            <center>
+                <br><br>
+                <h2> JOIN US NOW!</h2>
+            <button class="button"><span>LOGIN</span></button>
+             <button class="button"><span>SIGN UP</span></button></center>
+        </div>
+        <div class="box3">
+           <div id="slider">
+                <figure>
+                     <img src="images/photo2.jpg" alt>
+                    <img src="images/3.JPG" alt>
+                    <img src="images/pne.gif" alt="PNE" style="width:335px;height:412px;">
+                    
+                    <img src="images/4.jpg" alt>
+                    <img src="images/silent.gif" alt="PNE" style="width:335px;height:412px;">
+                </figure>
+            </div>
+            </div>
+      
+       
+        <div class="box5">
+          <center> <ul>
+               <li><a href="#home">HOME</a></li>
+               <li><a href="#bands">BANDS</a></li>
+               <li><a href="#webjocks">WEB JOCKS</a></li>
+               <li><a href="#chat">CHAT</a></li>
+              <li><a href="#buzz">BUZZ</a></li>
+              <li><a href="#more">MORE</a></li>
+            </ul></center></div>
+    </div>
+    <div class="parallax" id="home"> </div>
+    <div style="height:450px;background-color:#333539;font-size:36px" id="portfolio">
+        <div class="gridcol"> 
+                <div> 
+            <img src="images/Public/Picture/aboutusphoto.png" style="padding-left: 90px; padding-top: 10px;"></div>
+                 <div> <h1 style="padding-left: 1px; padding-top: 40px; font-family: 'Fjalla One', sans-serif; color: #c1927e; font-size: 35px;">ABOUT US? </h1>
+          <div class="words" style="font-family: 'Archivo Narrow', sans-serif; font-size: 24px; padding: 1px; color: #ded4d1 ">It is a free online streaming site where members<br> can listen to unsigned, independent music. The <br> format is much like radio, where you cannot make  <br>a playlist, but rather listen to the predetermined<br> songs as programmed by the content manager.
+                  <br>  <br>  <button style="background-color: #c1927e;  color: #ded4d1; padding: 15px; border-radius: 20px; font-family: 'Fjalla One'; font-size: 15px; "> KNOW MORE</button></div>
+            </div>
+            </div>
+            </div>
+     <div class="parallax" id="home">  </div>
+    <div style="height:470px;background-color:#333539;font-size:36px" id="portfolio">        
+        <div class="gridcols"> 
+        
+             <!---UNA--->
+           <div> 
+            <div id="slider">
+                <figure>
+                    <img src="images/Public/Picture/francoalbum1.jpg" >
+                    <img src="images/Public/Picture/eheads.jpg" >
+                    <img src="images/Public/Picture/u1.jpg" >
+                     <img src="images/Public/Picture/ur1.jpg" >
+                    <img src="images/Public/Picture/francoalbum1.jpg" >
+                </figure>
+            </div>
+            </div>
+                <!---GITNA--->
+            <div> 
+            <div id="slider">
+                <figure>
+                    <img src="images/Public/Picture/franco.png" >
+                    <img src="images/Public/Picture/top.png" >
+                    <img src="images/Public/Picture/unique.png" >
+                    <img src="images/Public/Picture/ur.png" >
+                    <img src="images/Public/Picture/franco.png" >
+                </figure>
+            </div>
+            </div>
+       
+            <!---DULO--->
+             <div> 
+            <div id="slider">
+                <figure>
+                    <img src="images/Public/Picture/f2.jpg">
+                    <img src="images/Public/Picture/e2.jpg">
+                     <img src="images/Public/Picture/u2.jpg" >
+                     <img src="images/Public/Picture/ur2.jpg" >
+                      <img src="images/Public/Picture/f2.jpg">
+                </figure>
+            </div>
+            </div>
+        </div>
+ </div>
+   
+     <div class="parallax" id="home">  </div>
+     <div style="height:440px;background-color:#333539;font-size:36px" id="portfolio">    
+         <div class="gridcols"> 
+        
+             <!---UNA--->
+        <div> 
+            <video width="320" height="240" controls>
+                <source src="images/hello.mp4" type="video/mp4">
+                    <source src="images/hello.mp4" type="video/ogg">
+                Your browser does not support the video tag.
+               </video>
+                <div class="words" style="font-family: 'Fjalla One', sans-serif; font-size: 24px;  padding-left: 30%; color: #ded4d1  "> LOVE DRUG </div>
+                <img src="images/Public/Picture/lovedrug.png">
+            </div>
+                <!---GITNA--->
+            <div> 
+             <video width="320" height="240" controls>
+                <source src="images/Parokya%20Ni%20Edgar%20-%20Lagi%20Mong%20Tatandaan%20MV%20Teaser.mp4" type="video/mp4">
+                    <source src="images/Parokya%20Ni%20Edgar%20-%20Lagi%20Mong%20Tatandaan%20MV%20Teaser.mp4" type="video/ogg">
+                Your browser does not support the video tag.
+               </video>
+                 <div class="words" style="font-family: 'Fjalla One', sans-serif; font-size: 24px;  padding-left: 30%; color: #ded4d1  "> OLD SOUL </div>
+                <img src="images/Public/Picture/lovedrug.png">
+            </div>
+             
+       
+            <!---DULO--->
+               <div> 
+            <video width="320" height="240" controls>
+                <source src="images/hello.mp4" type="video/mp4">
+                    <source src="images/hello.mp4" type="video/ogg">
+                Your browser does not support the video tag.
+               </video>
+                <div class="words" style="font-family: 'Fjalla One', sans-serif; font-size: 24px;  padding-left: 30%; color: #ded4d1  "> BING SHOW </div>
+                   <img src="images/Public/Picture/lovedrug.png">
+            </div>
+            
+        </div>
+    </div>
+    
+         <div class="parallax2" id="home">  </div>
+    <div style="height:250px;background-color:#333539;font-size:36px" id="portfolio"> 
+        <br>  <center> <div class="words" style="font-family: 'Fjalla One', sans-serif; font-size: 36px; color: #b78373  "> NewsLetter!</div>   
+        <div class="words" style="font-family: 'Archivo Narrow', sans-serif; font-size: 16px; color: #b78373  "> Get the latest news and updates on Bootleg Radio. Subscribe now!</div>  
+        <input type="text" id="fname" name="fname"> <br>
+        <button class="button"><span>JOIN!</span></button> 
+        </center> 
+        </div>
+     <div class="parallax2" id="home">  </div>
+    <div style="height:250px;background-color:##333539;font-size:36px" id="portfolio">
+    <div class="gridcolss"> 
+        <div> <div class="lalagyan" style="font-family: 'Yanone Kaffeesatz', sans-serif; color: #d49d96; font-size: 25px; padding-top: 35px; padding-left: 65px;"> Who's Your Daddy?</div>
+        <div class="lalagyan" style="font-family: 'Archivo Narrow'; color: #fce4e1; font-size: 25px; padding-left: 65px;"> Bootleg Radio</div>
+        
+        <div class="lalagyan" style="font-family: 'Archivo Narrow'; color: #dbc7c4; font-size: 19px; padding-left: 65px;"> 11th Flr. Common Goal    Tower Bldg.
+            Madrigal Business Park
+            Alabang, Muntinlupa City, Philippines
+            Email us: info@bootleg-radio.com</div> </div>
+        
+            <div> <div class="lalagyan" style="font-family: 'Yanone Kaffeesatz', sans-serif; color: #d49d96; font-size: 25px; padding-top: 35px; padding-left: 85px;"> To promote your products in Bootleg-Radio.com's site, please contact our sales group: </div>
+            <div class="lalagyan" style="font-family: 'Archivo Narrow'; color: #dbc7c4; font-size: 19px; padding-left: 85px;"> Phone: +63 (2) 772-3152 / 5859670
+Fax: +63 (2) 771-0143
+E-mail address: sales@bootleg-radio.com</div> 
+            </div>
+        <div> <div class="lalagyan" style="font-family: 'Yanone Kaffeesatz', sans-serif; color: #d49d96; font-size: 25px; padding-top: 35px; padding-left: 75px;"> For event sponsorships: </div>
+            <div class="lalagyan" style="font-family: 'Archivo Narrow'; color: #dbc7c4; font-size: 19px; padding-left: 75px;"> Please send proposals to sales@bootleg-radio.com</div>      
+        </div>
+        </div>
+    </div>
+    
+    <div class="footer" style=" position: relative; left: 0;  bottom: 0;  height: 30px;  width: 100%; background-color: #1b1b1c; color: white; text-align: center; padding-top: 10px;">  Copyright © 2018 Bootleg-Radio.com | About Us | Terms of Use | Email Us | FAQs </div>
     </body>
 </html>
