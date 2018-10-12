d539ab6fc956039bce749acb370ba8c50b73834d
Lovely <ellaendozo457@gmail.com>
2018-10-12 15:33:45 +0800

    Error changed

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('bandregister', array('as' => 'bandregister', 'uses' => 'HomeController@bandreg'));
 
-<<<<<<< HEAD
-Route::get('bandprofile', array('as' => 'bandprofile', 'uses' => 'HomeController@bandprof'));
-=======
 Route::resource('bands', 'BandRegController')->middleware('auth');
 
 Route::get('bands.profile', array('as' => 'bands.profile', 'uses' => 'BandRegController@index'))->middleware('auth');
->>>>>>> 862947692afc6d377d5f00d66a1786a0cf970801
+
