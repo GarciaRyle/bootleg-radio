b23e8830445e5ac58d1f315a01aa0c8258867b38
Lovely <ellaendozo457@gmail.com>
2018-10-12 13:16:35 +0800

    Band Profile UI

--- bootleg-radio/storage/framework/views/13e25cbba0679adc5861dde86cd80f8a405a17e2.php
+++ bootleg-radio/storage/framework/views/13e25cbba0679adc5861dde86cd80f8a405a17e2.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang=eng>
+
 <head>
     <title>Bootleg</title>
     <meta charset="UTF-8">
@@ -11,28 +12,158 @@
     <link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
 
-<body style="background-image: url(images/Image/registerbg.jpg); background-size: cover;" />
-<nav class="stroke">
-    <div class="nav">
-        <div class="nav-header">
-            <ul>
-                <li><img src="images/Image/logo.png" s style="width: 190px; height: 53px; padding-left: 40px; bottom: 10%;">
-                </li>
-                <li><a href="#home">HOME</a></li>
-                <li><a href="#bands">BANDS</a></li>
-                <li><a href="#webjocks">WEB JOCKS</a></li>
-                <li><a href="#chat">CHAT</a></li>
-                <li><a href="#buzz">BUZZ</a></li>
-                <li><a href="#more">MORE</a></li>
+<body style="background-color: #2e2b29">
+    <nav class="stroke">
+        <div class="nav">
+            <div class="nav-header">
+                <ul>
+                    <li><img src="images/Image/logo.png" s style="width: 190px; height: 53px; padding-left: 40px; bottom: 10%;">
+                    </li>
+                    <li><a href="#home">HOME</a></li>
+                    <li><a href="#bands">BANDS</a></li>
+                    <li><a href="#webjocks">WEB JOCKS</a></li>
+                    <li><a href="#chat">CHAT</a></li>
+                    <li><a href="#buzz">BUZZ</a></li>
+                    <li><a href="#more">MORE</a></li>
 
-            </ul>
+                </ul>
+
+            </div>
+        </div>
+
+    </nav>
+    <!--Profile-->
+    <div class="bandprofile">
+        <div class="box1">
+            <div>
+                <br>&nbsp;&nbsp;&nbsp;<img class="img-valign" src="images/Image/trialpic.jpg" alt="" style="  border-radius: 50%; width: 130px; height: 90px;" />
+                <span class="text2">&nbsp Band Name</span>
+                <div class="text3">&nbsp Genre</div><br><br>
+                <hr color: #c2b396>
+                <div class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tincidunt mauris, sit amet tristique orci. Vestibulum lacinia dui turpis, vitae finibus lorem vehicula vel. Ut a est sit amet mi consectetur venenatis</div> <br><br>
+
+                <center><p style="color: #141311; font-size: 20px; font-family: 'Lato', sans-serif;"> BAND MEMBERS</p></center>
+
+                <div class="bandmem">
+                    <div>
+                        <div class="containerreg">
+                            <img src="images/Image/vocalist.jpg" alt="Avatar" class="image">
+                            <div class="overlay">
+                                <div class="text">Vocalist</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="containerreg">
+                            <img src="images/Image/drum.jpg" alt="Guirtarist" class="image">
+                            <div class="overlay">
+                                <div class="text">Drummer</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="containerreg">
+                            <img src="images/Image/guitarist.jpg" alt="Guirtarist" class="image">
+                            <div class="overlay">
+                                <div class="text">Electric Guitarist</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="containerreg">
+                            <img src="images/Image/bass.jpg" alt="Avatar" class="image">
+                            <div class="overlay">
+                                <div class="text">Bass Guitarist</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="containerreg">
+                            <img src="images/Image/lead.jpg" alt="Avatar" class="image">
+                            <div class="overlay">
+                                <div class="text">Lead Singer</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+
+        <div class="box2">
+            <div class="album">
+                <div>
+                    <div class="containerreg">
+                        <img src="images/Image/album1.jpg" alt="Avatar" class="image">
+                        <div class="overlay">
+                            <div class="text">Album 1</div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="containerreg">
+                        <img src="images/Image/album2.jpg" alt="Avatar" class="image" style="height: 50%;">
+                        <div class="overlay">
+                            <div class="text">Album 2</div>
+                        </div>
+                    </div>
+                    <h2 style="font-size: 19px; text-align: center;">ARTIST ALBUM</h2>
+                </div>
+                <div>
+                    <div class="containerreg">
+                        <img src="images/Image/album3.jpg" alt="Avatar" class="image">
+                        <div class="overlay">
+                            <div class="text">Album 3</div>
+                        </div>
+                    </div>
+
+                    <h2 style="font-size: 19px; text-align: center;">ARTIST ALBUM</h2>
+                </div>
+
+            </div>
+
+            <!--album-->
+            <div class="gridcol">
+                <div>
+                    <label style="color: #a2321a; font-family: 'Lato', sans-serif;">Album Name:</label><br><br>
+                    <center>
+                        <div contenteditable="true" style="font-family: 'Lato', sans-serif; color: #c2b396; font-size: 19px; background-color: #ffffff; width: 250px; ">
+                            I BELONG TO THE ZOO
+                        </div>
+                    </center>
+
+                    <div class="form-group">
+                        <br> <label style="color: #a2321a; font-family: 'Lato', sans-serif; font-size: 17px;">Single Name:</label><br><br>
+                        <center>
+                            <div contenteditable="true" style="font-family: 'Lato', sans-serif; color: #c2b396; font-size: 19px; background-color: #ffffff; width: 250px; height: 30px; ">
+                                I BELONG TO THE ZOO
+                            </div>
+                        </center>
+                    </div>
+                </div>
+                <div>
+                    <div class="form-group">
+                        <div class="form-group">
+                            <label style="color: #a2321a; font-family: 'Lato', sans-serif;">Album Photo: </label><br>
+                            <center> <br><input type="file" name="pic" accept="image/*" style="font-family: 'Lato', sans-serif;"></center>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-group">
+                            <br><label style="color: #a2321a; font-family: 'Lato', sans-serif;">Single Photo: </label> <br>
+                            <center> <br><input type="file" name="pic" accept="image/*" style="font-family: 'Lato', sans-serif;"></center>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
 
         </div>
     </div>
-
-</nav>
-
-
 
     <!--sidebar code -->
     <div id="sidebar">
@@ -77,7 +208,6 @@
         <span></span>
         <span></span>
     </div>
-
     <!--javascript -->
     <script>
         function toggleSidebar(ref) {
@@ -93,7 +223,6 @@
 
 
 
-
-</div>
+</body>
 
 </html>
